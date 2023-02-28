bb6b11d422d6b951864d2418fb75e0925bb36c9b
SmaxxxLegs <artem.mysterio@gmail.com>
2023-02-28 22:50:45 +0100

    etap1.3

--- LibararyWebApplication/Models/Category.cs
+++ LibararyWebApplication/Models/Category.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibararyWebApplication.Models;
 
 public partial class Category
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage ="Field shouldn't be empty")]
+    [Display(Name="Category")]
     public string Name { get; set; } = null!;
-
     public string? Description { get; set; }
 
     public virtual ICollection<Product> Products { get; } = new List<Product>();
+
 }
